c9da6e41341faba17cdf56db02eb6d4d6c59368b
tanvirahmed055 <ahmedtanvir6168@gmail.com>
2021-11-10 07:01:43 +0600

    added responsive footer

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Footer from '../../Shared/Footer/Footer';
 import Header from '../../Shared/Header/Header';
 import Banner from '../Banner/Banner';
 import Products from '../Products/Products';
@@ -9,6 +10,7 @@ const Home = () => {
             <Header></Header>
             <Banner></Banner>
             <Products></Products>
+            <Footer></Footer>
         </div>
     );
 };
---
+++ src/Pages/Shared/Footer/Footer.js
@@ -0,0 +1,98 @@
+import React from 'react';
+import { Col, Container, FormControl, InputGroup, Button, Row, Form } from 'react-bootstrap';
+import './Footer.css';
+import { HashLink } from 'react-router-hash-link';
+
+const Footer = () => {
+    return (
+        <div className="mt-5  text-white" style={{ backgroundColor: 'RGB(39, 39, 39)', maxWidth: '100vw' }}>
+            <Container className="pt-4">
+                <Row>
+                    <Col sm={4}>
+                        <h3 className="fw-bolder mb-3 text-warning">FlyXDrone - A Premium Drone Provider</h3>
+                        <p className="mb-3">Our drones are extremely easy to fly. The drone becomes disconnected from the controller during flight the drone will autonomously fly back to where it took off. These drones also have intelligent flight modes such as active track, tapfly, and hand gesture control.</p>
+                        <div className="icons text-center mb-3">
+                            <i className="fab fa-facebook-square fa-2x icon facebook-icon-color me-2"></i>
+                            <i className="fab fa-twitter-square fa-2x icon twitter-icon-color me-2"></i>
+                            <i className="fab fa-linkedin fa-2x icon linkedin-icon-color me-2"></i>
+                            <i className="fab fa-youtube-square fa-2x icon youtube-icon-color me-2"></i>
+                            <i className="fab fa-whatsapp-square fa-2x icon whatsapp-icon-color me-2"></i>
+                        </div>
+                    </Col>
+                    <Col sm={2}>
+                        <h3 className="text-center text-warning fw-bold mb-3">Useful Links</h3>
+                        <ul>
+
+                            <HashLink className="me-2 nav-link pb-3" style={{ color: 'white' }} to="/home#services"><i className="fas fa-check"></i>&nbsp;Products</HashLink>
+
+                            <HashLink className="me-2 nav-link pb-3" style={{ color: 'white' }} to="/home#features"><i className="fas fa-check"></i>&nbsp;Features</HashLink>
+
+                            <HashLink className="me-2 nav-link pb-3" style={{ color: 'white' }} to="/home#clients"><i className="fas fa-check"></i>&nbsp;Reviews</HashLink>
+                        </ul>
+
+                    </Col>
+
+
+                    <Col sm={3}>
+                        <h3 className="mb-4 text-warning fw-bold ">Contact Us</h3>
+                        <div className="footer-addresss mb-3 text-center">
+                            <i className="fas fa-map-marker-alt"></i>&nbsp;
+                            12, Mirpur, Dhaka, Bangladesh.
+                        </div>
+
+                        <div className="footer-phone mb-3 text-center">
+
+                            <i className="fas fa-envelope"></i>&nbsp;
+                            admin1_drone@yahoo.com
+                            <br />
+                            <i className="fas fa-envelope"></i>&nbsp;
+                            admin2_drone@gmail.com
+                        </div>
+
+                        <div className="footer-phone mb-3 text-center">
+
+                            <i className="fas fa-phone"></i>&nbsp;
+                            +881-54647835
+                            <br />
+                            <i className="fas fa-phone"></i>&nbsp;
+                            +881-54647835
+                        </div>
+
+
+                    </Col>
+                    <Col sm={3}>
+                        <h3 className="mb-3 text-warning fw-bold">Stay In Touch</h3>
+                        <Form>
+                            <Form.Group className="mb-1" controlId="exampleForm.ControlInput1">
+                                <Form.Control type="text" placeholder="Name" />
+
+                            </Form.Group>
+                            <Form.Group className="mb-1" controlId="exampleForm.ControlInput1">
+
+                                <Form.Control type="Email" placeholder="name@example.com" />
+                            </Form.Group>
+                            <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                                <Form.Control as="textarea" rows={3} placeholder="Message" />
+                            </Form.Group>
+                            <div className="d-flex justify-content-center">
+                                <Button variant="danger" className="px-5">Send</Button>{' '}
+                            </div>
+                        </Form>
+
+
+                    </Col>
+                </Row>
+                <Row>
+                    <Col className="d-flex flex-column align-self-end justify-content-center pb-1  fw-bold">
+                        <hr />
+                        <h5 className="text-warning">Copyright © 2021 FlyXDrone, All Rights Reserved.
+
+                        </h5></Col>
+                </Row>
+            </Container>
+
+        </div>
+    );
+};
+
+export default Footer;
