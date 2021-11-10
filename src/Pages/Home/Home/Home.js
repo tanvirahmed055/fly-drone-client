4ccfdb553a0e5745dcd73d5c80cc44d977cb45a3
tanvirahmed055 <ahmedtanvir6168@gmail.com>
2021-11-10 18:21:25 +0600

    done with resposive features section

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Footer from '../../Shared/Footer/Footer';
 import Header from '../../Shared/Header/Header';
 import Banner from '../Banner/Banner';
+import Features from '../Features/Features';
 import Products from '../Products/Products';
 
 const Home = () => {
@@ -10,6 +11,7 @@ const Home = () => {
             <Header></Header>
             <Banner></Banner>
             <Products></Products>
+            <Features></Features>
             <Footer></Footer>
         </div>
     );
